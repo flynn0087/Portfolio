1d22bf336961599c993532e30bb7f32d7ab1b709
Ben Flynn <flynn0087@gmail.com>
2021-04-13 16:12:23 -0500

    navbar adjustment

--- src/App.js
+++ src/App.js
@@ -5,19 +5,16 @@ import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
 
-
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Container>
-          <Route exact path="/" component={About} />
-          <Route exact path="/Portfolio" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/myportfolio" component={myPortfolio} />
-        </Container>
-      </div>
+      <Navbar />
+      <Container>
+        <Route exact path="/" component={About} />
+        <Route exact path="/Portfolio" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/myportfolio" component={myPortfolio} />
+      </Container>
     </Router>
   );
 }
