7d132959ceaff23c4fda7ba6c998f6b924a04822
Ben Flynn <flynn0087@gmail.com>
2021-04-13 16:17:37 -0500

    navbar adjustment

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg color-nav container-fluid">
+    <nav className="navbar navbar-expand-lg color-nav">
       <Link className="navbar-brand" to="/" style={{ color: "gold" }}>
         Ben Flynn
       </Link>
